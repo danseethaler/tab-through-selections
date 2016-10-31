5cf085e13e61891d6cd84ffb6192c999bc1074f9
Dan Seethaler <dan@seethalers.com>
2016-10-30 19:12:00 -0600

    Initial commit

---
+++ spec/tab-through-selections-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TabThroughSelectionsView from '../lib/tab-through-selections-view';
+
+describe('TabThroughSelectionsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
